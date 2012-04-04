0a12abd70e04b7cd3703cb5770898279d04746d0
jdbermeol <jdbermeol@gmail.com>
2012-04-04 17:00:49 -0500

    ProfessorVo class completed. UserVo class completed. ProgramVo class completed. SubjectVo class completed.

--- src/main/java/org/xtremeware/iudex/vo/SubjectVo.java
+++ src/main/java/org/xtremeware/iudex/vo/SubjectVo.java
@@ -3,53 +3,67 @@ package org.xtremeware.iudex.vo;
 
 public class SubjectVo extends ValueObject{
 
-	private Long id;
-	private String name;
-	private String description;
+    private Long id;
+    private String name;
+    private String description;
 
-	@Override
-	public boolean equals(Object ob) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SubjectVo other = (SubjectVo) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.description == null) ? (other.description != null) : !this.description.equals(other.description)) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 41 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 41 * hash + (this.description != null ? this.description.hashCode() : 0);
+        return hash;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    @Override
+    public String toString() {
+        return "SubjectVo{" + "id=" + id + ", name=" + name + ", description=" + description + '}';
+    }
 
-	@Override
-	public int compareTo(ValueObject vo) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
--- src/main/java/org/xtremeware/iudex/vo/UserVo.java
+++ src/main/java/org/xtremeware/iudex/vo/UserVo.java
@@ -3,100 +3,133 @@ package org.xtremeware.iudex.vo;
 import org.xtremeware.iudex.helper.Role;
 
 public class UserVo extends ValueObject{
+    
+    private Long id;
+    private String firstName;	
+    private String lastName;
+    private String userName;
+    private String password;
+    private Role role;
+    private boolean active;
+    private Long programId;
 
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private String userName;
-	private String password;
-	private Role role;
-	private boolean active;
-	private ProgramVo program;
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UserVo other = (UserVo) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if ((this.firstName == null) ? (other.firstName != null) : !this.firstName.equals(other.firstName)) {
+            return false;
+        }
+        if ((this.lastName == null) ? (other.lastName != null) : !this.lastName.equals(other.lastName)) {
+            return false;
+        }
+        if ((this.userName == null) ? (other.userName != null) : !this.userName.equals(other.userName)) {
+            return false;
+        }
+        if ((this.password == null) ? (other.password != null) : !this.password.equals(other.password)) {
+            return false;
+        }
+        if (this.role != other.role) {
+            return false;
+        }
+        if (this.active != other.active) {
+            return false;
+        }
+        if (this.programId != other.programId && (this.programId == null || !this.programId.equals(other.programId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 53 * hash + (this.firstName != null ? this.firstName.hashCode() : 0);
+        hash = 53 * hash + (this.lastName != null ? this.lastName.hashCode() : 0);
+        hash = 53 * hash + (this.userName != null ? this.userName.hashCode() : 0);
+        hash = 53 * hash + (this.password != null ? this.password.hashCode() : 0);
+        hash = 53 * hash + (this.role != null ? this.role.hashCode() : 0);
+        hash = 53 * hash + (this.active ? 1 : 0);
+        hash = 53 * hash + (this.programId != null ? this.programId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "UserVo{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName + ", password=" + password + ", role=" + role + ", active=" + active + ", programId=" + programId + '}';
+    }
 	
+    public boolean isActive() {
+        return active;
+    }
 
-	@Override
-	public boolean equals(Object ob) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-	@Override
-	public int hashCode() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	@Override
-	public int compareTo(ValueObject vo) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getProgramId() {
+        return programId;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setProgramId(Long programId) {
+        this.programId = programId;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public Role getRole() {
+        return role;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public ProgramVo getProgram() {
-		return program;
-	}
-
-	public void setProgram(ProgramVo program) {
-		this.program = program;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
 }

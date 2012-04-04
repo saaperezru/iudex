3d6fb461c44ebefd69199db35f9190f4ece2e5c1
Juan Hernández <jfhernandeza8@gmail.com>
2012-04-04 18:24:24 -0500

    FeedbackEntity and PeriodEntity implemented

--- src/main/java/org/xtremeware/iudex/entity/PeriodEntity.java
+++ src/main/java/org/xtremeware/iudex/entity/PeriodEntity.java
@@ -1,50 +1,89 @@
-
 package org.xtremeware.iudex.entity;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import org.xtremeware.iudex.vo.PeriodVo;
 
-@Entity
-public class PeriodEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+@javax.persistence.Entity
+public class PeriodEntity implements Serializable, Entity<PeriodVo> {
 
-	public Long getId() {
-		return id;
-	}
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private int year;
+    private int semester;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public PeriodVo toVo(){
+        PeriodVo vo = new PeriodVo();
+        
+        vo.setId(id);
+        vo.setSemester(semester);
+        vo.setYear(year);
+        
+        return vo;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (id != null ? id.hashCode() : 0);
-		return hash;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PeriodEntity other = (PeriodEntity) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if (this.year != other.year) {
+            return false;
+        }
+        if (this.semester != other.semester) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		// TODO: Warning - this method won't work in the case the id fields are not set
-		if (!(object instanceof PeriodEntity)) {
-			return false;
-		}
-		PeriodEntity other = (PeriodEntity) object;
-		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 97 * hash + this.year;
+        hash = 97 * hash + this.semester;
+        return hash;
+    }
 
-	@Override
-	public String toString() {
-		return "org.xtremeware.iudex.entity.PeriodEntity[ id=" + id + " ]";
-	}
+    @Override
+    public String toString() {
+        return "PeriodEntity{" + "id=" + id + ", year=" + year + ", semester=" + semester + '}';
+    }
+    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
 }

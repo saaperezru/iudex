dcd9ea4951dd3c92d130d6d1b7169b13a1cf3a73
Diego Gerena <dagerenaq@gmail.com>
2012-04-04 19:23:36 -0500

    Fixed problem on Classes SubjectRatingEntity , ProfessorRatingEntity due to import javax.persistence.Entity;

--- src/main/java/org/xtremeware/iudex/entity/ProfessorRatingEntity.java
+++ src/main/java/org/xtremeware/iudex/entity/ProfessorRatingEntity.java
@@ -2,7 +2,6 @@
 package org.xtremeware.iudex.entity;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
